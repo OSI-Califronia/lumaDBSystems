8287918c1b5b40dbab7f3d66c33c5492b0c84942
lukaseckert <lukas.eckert@htwg-konstanz.de>
2012-12-20 15:22:50 +0100

    added countrybean

---
+++ src/data/CountryBean.java
@@ -0,0 +1,26 @@
+package data;
+
+public class CountryBean {
+	
+	private String ISOLand;
+	private String Name;
+	
+	public String getISOLand() {
+		return ISOLand;
+	}
+	public void setISOLand(String iSOLand) {
+		ISOLand = iSOLand;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getISOLand() + " " + this.getName();
+	}
+
+}
